a41a15477a95804513a938a3439ef38546de90a0
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2020-11-21 00:25:52 +0530

    Commented out numberbox

--- src/Components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/Components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -8,7 +8,7 @@ const buildControl = (props) => {
    
             <div className={classes.Label}>{props.label}</div>
             <button className={classes.Less} onClick={props.removed} disabled={props.disabled}>-</button>
-            <div className={classes.numberBox}>1</div>
+            {/* <div className={classes.numberBox}>1</div> */}
             <button className={classes.More} onClick={props.added}>+</button>
             </div>
         </div>
